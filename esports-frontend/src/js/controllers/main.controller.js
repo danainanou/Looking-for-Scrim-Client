0721fc018e3e2ac85c426d7abdf958285600aba7
danainanou <danai.nanou@gmail.com>
2017-06-05 15:45:17 +0100

    basic angular authentication done

---
+++ esports-frontend/src/js/controllers/main.controller.js
@@ -0,0 +1,22 @@
+angular
+  .module('esports-app')
+  .controller('MainCtrl', MainCtrl);
+
+MainCtrl.$inject = ['$rootScope', 'CurrentUserService', '$state'];
+function MainCtrl($rootScope, CurrentUserService, $state) {
+  const vm = this;
+
+  $rootScope.$on('loggedIn', () => {
+    $rootScope.user = CurrentUserService.currentUser;
+  });
+
+  $rootScope.$on('loggedOut', () => {
+    vm.user = null;
+    $state.go('login');
+  });
+
+  vm.logout = () => {
+    CurrentUserService.removeUser();
+  };
+
+}
